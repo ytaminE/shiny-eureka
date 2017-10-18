55eb5155c0554a9828cc24a405c5c70c0797204b
Yuanyi Yang <yuanyi.yang@foxmail.com>
2017-10-18 01:05:58 -0400

    Clan the code in views.py

--- app/views.py
+++ app/views.py
@@ -10,18 +10,21 @@ from wand.image import Image
 from . import app, db, login_manager
 from .models import User, Photo
 
+# Restriction on image types
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif', 'JPG', 'PNG', 'JPEG', 'GIF'}
 
 
+# Route for the welcome page
 @app.route('/')
 @app.route('/welcome')
 def welcome():
     if current_user.is_authenticated:
         return redirect('home')
     else:
-        return render_template('welcome.html')  # render a template
+        return render_template('welcome.html')
 
 
+# Route for the home page (used for showing pictures)
 @app.route('/home')
 @login_required
 def home():
@@ -37,10 +40,7 @@ def home():
     return render_template('home.html', images=images, user_id=user_id)
 
 
-def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
-
+# Route for empty images action
 @app.route('/empty', methods=['GET', 'POST'])
 @login_required
 def empty():
@@ -58,6 +58,7 @@ def empty():
     return redirect('home')
 
 
+# Route for upload function
 @app.route('/upload', methods=['GET', 'POST'])
 @login_required
 def upload():
@@ -72,53 +73,17 @@ def upload():
 
         file = request.files['uploadedfile']
 
-        # if user does not select file, browser also submit a empty part without filename
         if file.filename == '':
             return redirect(request.url)
 
         if file and allowed_file(file.filename):
-            # Pass it a filename and it will return a secure version of it.
-            # This filename can then safely be stored on a regular file system and passed to os.path.join().
-            # The filename returned is an ASCII only string for maximum portability.
-            filename = secure_filename(file.filename)
-
-            save_path = app.config['UPLOAD_FOLDER'] + '/' + user_id + '/'
-            t1_path = save_path + 'resize/'
-            t2_path = save_path + 'rotate/'
-            t3_path = save_path + 'enhancement/'
-
-            if not os.path.exists(save_path):
-                os.makedirs(save_path)
-            if not os.path.exists(t1_path):
-                os.makedirs(t1_path)
-            if not os.path.exists(t2_path):
-                os.makedirs(t2_path)
-            if not os.path.exists(t3_path):
-                os.makedirs(t3_path)
-
-            file.save(os.path.join(save_path, filename))
-
-            with Image(filename=save_path + filename) as img:
-                with img.clone() as i:
-                    i.resize(int(i.width * 0.7), int(i.height * 0.7))
-                    i.save(filename=t1_path + filename)
-                with img.clone() as i:
-                    i.rotate(90)
-                    i.save(filename=t2_path + filename)
-                with img.clone() as i:
-                    i.evaluate(operator='rightshift', value=1, channel='red')
-                    i.save(filename=t3_path + filename)
-
-            image = Photo(filename, username, save_path, t1_path, t2_path, t3_path)
-            db.session.add(image)
-            db.session.commit()
-            # return redirect(url_for('uploaded_file',filename=filename))
+            processAndStoreImage(file, username, user_id)
             return redirect(url_for('home'))
 
     return redirect('home')
 
 
-# route for handling the login page logic
+# Route for handling the login page logic
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     error = None
@@ -135,6 +100,7 @@ def login():
     return render_template('login.html', error=error)
 
 
+# Route for logout
 @app.route('/logout')
 @login_required
 def logout():
@@ -142,11 +108,13 @@ def logout():
     return render_template('welcome.html')
 
 
+# Route for register page
 @app.route('/register')
 def register():
     return render_template('register.html')
 
 
+# Route for register submit
 @app.route('/register_submit', methods=['GET', 'POST'])
 def register_submit():
     error = 'Username already registered'
@@ -164,16 +132,13 @@ def register_submit():
     return render_template('register.html', error=error)
 
 
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(user_id)
-
-
+# TA test page
 @app.route('/uploadForm', methods=['GET', 'POST'])
 def uploadForm():
     return render_template('uploadForm.html')
 
 
+# Used for TA test
 @app.route('/test/FileUpload', methods=['POST'])
 def test_upload():
     error = None
@@ -185,53 +150,65 @@ def test_upload():
         if user is not None and user.is_correct_password(password):
             login_user(user)
             user_id = current_user.get_id()
+            # If user does not select file, browser also submit a empty part without filename
             if file.filename == '':
                 return redirect(request.url)
             if file and allowed_file(file.filename):
-                filename = secure_filename(file.filename)
-                save_path = app.config['UPLOAD_FOLDER'] + '/' + user_id + '/'
-                t1_path = save_path + 'resize/'
-                t2_path = save_path + 'rotate/'
-                t3_path = save_path + 'enhancement/'
-
-                if not os.path.exists(save_path):
-                    os.makedirs(save_path)
-                if not os.path.exists(t1_path):
-                    os.makedirs(t1_path)
-                if not os.path.exists(t2_path):
-                    os.makedirs(t2_path)
-                if not os.path.exists(t3_path):
-                    os.makedirs(t3_path)
-
-                file.save(os.path.join(save_path, filename))
-                with Image(filename=save_path + filename) as img:
-                    with img.clone() as i:
-                        i.resize(int(i.width * 0.7), int(i.height * 0.7))
-                        i.save(filename=t1_path + filename)
-                    with img.clone() as i:
-                        i.rotate(90)
-                        i.save(filename=t2_path + filename)
-                    with img.clone() as i:
-                        i.evaluate(operator='rightshift', value=1, channel='red')
-                        i.save(filename=t3_path + filename)
-                image = Photo(filename, username, save_path, t1_path, t2_path, t3_path)
-                db.session.add(image)
-                db.session.commit()
+                processAndStoreImage(file, username, user_id)
                 return redirect(url_for('uploadForm'))
         else:
             error = 'Username or password is incorrect. Please try again.'
     return render_template('uploadForm.html', error=error)
 
-# # routes for testing database
-# @app.route('/testdb')
-# def testdb():
-#     try:
-#         print("----------------------")
-#         for user in User.query.all():
-#             print(user)
-#         # db.session.query("1").from_statement("SELECT 1").all()
-#         print("----------------------")
-#         return '<h1>It works.</h1>'
-#     except:
-#         raise
-#         return '<h1>Something is broken.</h1>'
+
+# Process the original image and save the image with its transformations
+def processAndStoreImage(file, username, user_id):
+    # Pass it a filename and it will return a secure version of it.
+    # This filename can then safely be stored on a regular file system and passed to os.path.join().
+    # The filename returned is an ASCII only string for maximum portability.
+    filename = secure_filename(file.filename)
+    save_path = app.config['UPLOAD_FOLDER'] + '/' + user_id + '/'
+
+    t1_path = save_path + 'resize/'
+    t2_path = save_path + 'rotate/'
+    t3_path = save_path + 'enhancement/'
+
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
+    if not os.path.exists(t1_path):
+        os.makedirs(t1_path)
+    if not os.path.exists(t2_path):
+        os.makedirs(t2_path)
+    if not os.path.exists(t3_path):
+        os.makedirs(t3_path)
+
+    # Save the original image into the server
+    file.save(os.path.join(save_path, filename))
+    with Image(filename=save_path + filename) as img:
+        # First transformation
+        with img.clone() as i:
+            i.resize(int(i.width * 0.7), int(i.height * 0.7))
+            i.save(filename=t1_path + filename)
+        # Second transformation
+        with img.clone() as i:
+            i.rotate(90)
+            i.save(filename=t2_path + filename)
+        # Third transformation
+        with img.clone() as i:
+            i.evaluate(operator='rightshift', value=1, channel='red')
+            i.save(filename=t3_path + filename)
+
+    # Store the path information into the database
+    image = Photo(filename, username, save_path, t1_path, t2_path, t3_path)
+    db.session.add(image)
+    db.session.commit()
+
+
+# A helper function used to add restriction on file type
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
