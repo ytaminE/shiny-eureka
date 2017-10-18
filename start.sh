740466106d23704466d6fb773a4dce12cf8d88f1
Ubuntu <ubuntu@ip-172-31-23-54.ec2.internal>
2017-10-18 05:03:08 +0000

    Update the start.sh with port binding

--- start.sh
+++ start.sh
@@ -1,2 +1,3 @@
-export APP_CONFIG_FILE=development.py
+./venv/bin/gunicorn --bind 0.0.0.0:5000 --workers=1 --access-logfile access.log --error-logfile error.log app:app
+export APP_CONFIG_FILE=production.py
 python run.py
